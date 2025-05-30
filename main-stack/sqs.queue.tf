a3fcfe14499dcb864305d221365afaf4155dce13
Elvis <elvisfnunes@gmail.com>
2025-05-30 12:35:43 -0300

    Automatizando a criação de workspaces

--- main-stack/sqs.queue.tf
+++ main-stack/sqs.queue.tf
@@ -1,6 +1,6 @@
 resource "aws_sqs_queue" "this" {
   count = length(var.queue)
-  name                      = var.queue[count.index].name
+  name                      = "${var.queue[count.index].name}-${terraform.workspace}"
   delay_seconds             = var.queue[count.index].delay_seconds
   max_message_size          = var.queue[count.index].max_message_size
   message_retention_seconds = var.queue[count.index].message_retention_seconds
